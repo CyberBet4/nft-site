a47255e0e2117b5bd45dd1c7ca89c2b43fea41f0
CyberBet4 <triumphanazia@gmail.com>
2022-09-18 22:48:52 +0100

    Added Tawk.to widget

--- resources/views/unauth/layouts/header.blade.php
+++ resources/views/unauth/layouts/header.blade.php
@@ -34,6 +34,19 @@
     <link rel="stylesheet" type="text/css" href={{ asset("css/SFRht5YUKyXp.css")}}>
 </head>
 <body>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6327900e37898912e969eb62/1gd98lehc';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <!--Preloader area start here-->
     <section class="loader_first">
         <div class="loader_first_inner">
